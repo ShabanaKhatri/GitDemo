2af69fe613e472e62f273eeb7c57d2b751e168ab
Shabana <Shabana>
2016-12-15 00:46:55 +0530

    Initial commit

---
+++ app/src/main/java/com/abc/demo/MainActivity.java
@@ -0,0 +1,17 @@
+package com.abc.demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private void uploadtoGit() {
+        int a = 1 + 2;
+    }
+}
